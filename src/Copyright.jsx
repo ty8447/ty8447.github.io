f72fe001616790ce56ec9caefb6f7fa07fac3f4d
ty8447 <32338679+ty8447@users.noreply.github.com>
2024-06-07 00:55:51 -0400

    Fixed Contact Section

--- src/Copyright.jsx
+++ src/Copyright.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Copyright = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="copyright" style={{color: "#132852"}}>
+    <footer className="copyright" style={{color: "#132852", backgroundColor: "#e3d8ad", marginBottom: "10px"}}>
       &copy; {currentYear} Cole Rabe. All rights reserved.
     </footer>
   );
